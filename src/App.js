402413b66a2a3e983b6838736b8d5cacb366e0e8
yashpatel-tatva <156052898+yashpatel-tatva@users.noreply.github.com>
2024-06-21 15:08:39 +0530

    kl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div>
+
     </div>
   );
 }
